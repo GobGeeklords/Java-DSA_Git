260ace90457f4c64c83b7c7dae4ece82dfd038c9
GobGeeklords <110241119+GobGeeklords@users.noreply.github.com>
2023-09-06 13:42:52 +0530

    Insertion Sort

---
+++ InsertionSort.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+public class InsertionSort {
+    public static void main(String[] args) {
+        int [] arr={5,3,4,1,2};
+        int [] ans=Sort(arr);
+        System.out.println(Arrays.toString(ans));
+    }
+    public static  int[] Sort(int [] arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            for(int j=i+1;j>0;j--){
+                if (arr[j]<arr[j-1]){
+                    swap(arr,j,j-1);
+            }
+                else
+                    break;
+            }
+        }
+        return arr;
+    }
+    public static  void  swap(int[]arr, int first, int second){
+        int temp=arr[second];
+        arr[second]=arr[first];
+        arr[first]=temp;
+    }
+}
