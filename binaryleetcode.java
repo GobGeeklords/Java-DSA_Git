8967f5b06b6c662e12825170faa7eef3f7a73ac7
GobGeeklords <110241119+GobGeeklords@users.noreply.github.com>
2023-09-06 13:38:48 +0530

    Add files via upload
    Leetcode problem solved for binary search(best complexity)

---
+++ binaryleetcode.java
@@ -0,0 +1,25 @@
+public class binaryleetcode {
+    public static void main(String[] args) {
+        int[] nums={-1,0,3,5,9,12};
+        int target=9;
+        int ans=search(nums,target);
+        System.out.println(ans);
+    }
+    public static int  search(int[] nums, int target) {
+        int start=0;
+        int end=nums.length-1;
+        while (start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if (target<nums[mid])
+                start=mid+1;
+            else if(target>nums[mid])
+                end=mid-1;
+            else
+                return -1;
+        }
+        return -1;
+    }
+}
+
