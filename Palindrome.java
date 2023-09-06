07360b6e21e7af1705b2af9f849b443028406e4b
GobGeeklords <110241119+GobGeeklords@users.noreply.github.com>
2023-09-06 13:45:52 +0530

    Palindrome of a number

---
+++ Palindrome.java
@@ -0,0 +1,21 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        String name=null;// check whether it is palindrome or not
+        System.out.println(Palin(name));
+        }
+        public static boolean Palin( String str){
+        if(str ==null ||str.length()==0 ){
+            return true;
+        }
+        str=str.toLowerCase();
+            for (int i = 0; i < str.length()/2; i++) {
+                char start=str.charAt(i);
+                char end=str.charAt(str.length()-1-i);
+                if (start!=end){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
